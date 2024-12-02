2a5883459973ad4fe9bba8bb5f6bb47dcac46b2e
Nathan Bissett <nate@Nathans-Mac.local>
2024-12-02 13:21:08 -0500

    Added full project files

---
+++ FrontEnd/Tippsy/Tippsy/TippsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TippsyApp.swift
+//  Tippsy
+//
+//  Created by Nathan Bissett on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct TippsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrontEnd/Tippsy/TippsyTests/TippsyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TippsyTests.swift
+//  TippsyTests
+//
+//  Created by Nathan Bissett on 2024-11-18.
+//
+
+import Testing
+@testable import Tippsy
+
+struct TippsyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
